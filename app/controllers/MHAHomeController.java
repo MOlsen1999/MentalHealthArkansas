b9de6bcff68589fb6ac6168dee9f8f7e8d2d5408
marko <markolsen26@gmail.com>
2018-09-24 09:52:51 -0500

    Weird Error Checkin

--- app/controllers/MHAHomeController.java
+++ app/controllers/MHAHomeController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import play.data.FormFactory;
+import play.db.jpa.JPAApi;
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -8,20 +11,40 @@ import javax.inject.Inject;
 
 public class MHAHomeController extends Controller
 {
+    private FormFactory formFactory;
+
+    private JPAApi jpaApi;
+
     @Inject
+    public MHAHomeController(FormFactory formFactory, JPAApi jpaApi)
+    {
+        this.formFactory = formFactory;
+        this.jpaApi = jpaApi;
+    }
+
+
     public Result getMHAHome()
     {
         return ok(views.html.home.render());
     }
 
+    @Transactional
     public Result getProviderSearch()
     {
         return ok(views.html.providersearch.render());
     }
+
+    public Result postReturnInfo()
+    {
+       return ok(views.html.returnpage.render());
+    }
+
     public Result getProviderInfo()
     {
         return ok(views.html.iamaprovider.render());
     }
+
+
     public Result getReturnInfo()
     {
         return ok(views.html.returnpage.render());
--- app/models/ProviderLogIn.java
+++ app/models/ProviderLogIn.java
@@ -1,9 +1,11 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class ProviderLogIn
 {
+@Id String loging;
 
 }
